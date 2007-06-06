e66655d133b4d4d1555ddb81279eab5615a44bb7
Ludovic Court`es <ludovic.courtes@laas.fr>
2007-06-06 12:33:29 +0000

    Cleaned up the source code fontifiers (under `coloring').
    This brings `(skribilo coloring c)' to life!
    
    git-archimport-id: lcourtes@laas.fr--2006-libre/skribilo--devo--1.2--patch-63

--- src/guile/skribilo/coloring/xml-lex.scm
+++ src/guile/skribilo/coloring/xml-lex.scm
@@ -1,3 +1,11 @@
+(define-module (skribilo coloring xml-lex)
+  :use-module (skribilo lib)
+  :use-module (skribilo coloring parameters)
+  :export (lexer-init lexer
+           lexer-get-func-column
+           lexer-get-func-offset
+           lexer-get-line lexer-getc
+           lexer-ungetc))
 ; *** This file starts with a copy of the file multilex.scm ***
 ; SILex - Scheme Implementation of Lex
 ; Copyright (C) 2001  Danny Dube'
@@ -14,7 +22,7 @@
 ; 
 ; You should have received a copy of the GNU General Public License
 ; along with this program; if not, write to the Free Software
-; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+; Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 ;
 ; Gestion des Input Systems
