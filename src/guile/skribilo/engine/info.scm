3ad179abcc3eeda0b90c29bdaf5e16bf541a7c9e
Ludovic Courtès <ludo@gnu.org>
2008-10-09 00:16:01 +0200

    Fix Info engine module name.

--- src/guile/skribilo/engine/info.scm
+++ src/guile/skribilo/engine/info.scm
@@ -19,7 +19,7 @@
 ;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301,
 ;;; USA.
 
-(define-module (skribilo engine latex)
+(define-module (skribilo engine info)
   :use-module (skribilo lib)
   :use-module (skribilo ast)
   :use-module (skribilo engine)
